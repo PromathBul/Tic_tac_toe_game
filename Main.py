f84dd7bb0b359c7f85a597a3bf4151a9beec37a9
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-10-25 12:56:59 +0300

    Created Tic-tac-toe game

--- Main.py
+++ Main.py
@@ -1,44 +1,23 @@
+from Methods import print_board
+from Methods import move_player as move
+from Methods import control_win as control
+
 board = range(1,10)
 board = list(map(str, board))
-       
-def move_player(sign):
-    check = False
-    while not check:
-        move = input('Введите номер ячейки, куда поставить ' + sign + ': ')
-        try:
-            move = int(move)
-        except:
-            print('Нужно ввести число.')
-        if move >= 1 and move <= 9:
-            if str(board[move - 1]) not in 'XO':
-                board[move - 1] = sign
-                check = True
-            else:
-                print('Эта ячейка уже занята.')
-        else:
-            print('Введите число от 1 до 10.')
-           
-def control_win(board):
-    coordinates = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    for item in coordinates:
-        if board[item[0]] == board[item[1]] == board[item[2]]:
-            return board[item[0]]
-    return False
-   
-
 
 count = 0
 win = False
+
 while not win:
     print_board(board)
     if count % 2 == 0:
-        move_player('X')
+        move('X', board)
     else:
         sign = 'O'
-        move_player(sign)
+        move(sign, board)
     count += 1
     if count > 4:
-        value = control_win(board)
+        value = control(board)
         if value:
             print(value + ' выиграл.')
             win = True
--- Methods.py
+++ Methods.py
@@ -3,3 +3,27 @@ def print_board(board):
     for i in range(3):
         print('|', board[i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
         print ('-------------')
+
+def move_player(sign, board):
+    check = False
+    while not check:
+        move = input('Введите номер ячейки, куда поставить ' + sign + ': ')
+        try:
+            move = int(move)
+        except:
+            print('Нужно ввести число.')
+        if move >= 1 and move <= 9:
+            if str(board[move - 1]) not in 'XO':
+                board[move - 1] = sign
+                check = True
+            else:
+                print('Эта ячейка уже занята.')
+        else:
+            print('Введите число от 1 до 9.')
+
+def control_win(board):
+    coordinates = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for item in coordinates:
+        if board[item[0]] == board[item[1]] == board[item[2]]:
+            return board[item[0]]
+    return False
